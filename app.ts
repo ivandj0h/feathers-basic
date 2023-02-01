ad2fd3acbd295e55e4f8d70c595c9a7fbae9b2e6
ivandjoh <ivandi.djoh@gmail.com>
2023-02-01 17:53:43 +0800

    html added

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
 import { feathers } from '@feathersjs/feathers'
+import { koa, rest, bodyParser, errorHandler, serveStatic } from '@feathersjs/koa'
+import socketio from '@feathersjs/socketio'
 
 // This is the interface for the message data
 interface Message {
@@ -26,6 +28,7 @@ class MessageService {
 
     // Add new message to the list
     this.messages.push(message)
+    console.log(message)
 
     return message
   }
@@ -36,33 +39,33 @@ type ServiceTypes = {
   messages: MessageService
 }
 
-const app = feathers<ServiceTypes>()
+// Creates an ExpressJS compatible Feathers application
+const app = koa<ServiceTypes>(feathers())
 
-// Register the message service on the Feathers application
+// Use the current folder for static file hosting
+app.use(serveStatic('.'))
+// Register the error handle
+app.use(errorHandler())
+// Parse JSON request bodies
+app.use(bodyParser())
+
+// Register REST service handler
+app.configure(rest())
+// Configure Socket.io real-time APIs
+app.configure(socketio())
+// Register our messages service
 app.use('messages', new MessageService())
 
-// Log every time a new message has been created
-app.service('messages').on('created', (message: Message) => {
-  console.log('A new message has been created', message)
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', (connection) => app.channel('everybody').join(connection))
+// Publish all events to the `everybody` channel
+app.publish((_data) => app.channel('everybody'))
+
+// Start the server
+app.listen(3030).then(() => console.log('Feathers server listening on localhost:3030'))
+
+// For good measure let's create a message
+// So our API doesn't look so empty
+app.service('messages').create({
+  text: 'Hello world from the server'
 })
-
-// A function that creates messages and then logs
-// all existing messages on the service
-const main = async () => {
-  // Create a new message on our message service
-  await app.service('messages').create({
-    text: 'Hello Feathers'
-  })
-
-  // And another one
-  await app.service('messages').create({
-    text: 'Hello again'
-  })
-
-  // Find all existing messages
-  const messages = await app.service('messages').find()
-
-  console.log('All messages', messages)
-}
-
-main()
